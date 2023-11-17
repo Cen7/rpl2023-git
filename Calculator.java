b2000c7491dbf27cd7528b137933ab0374c58b29
Nathaniel Vincent <nathanielvincentnv@gmail.com>
2023-11-17 23:14:50 +0700

    Revert "tambah method minus'"
    This reverts commit e77da10873ba27caf303111db93b21c5205e6f52.

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,7 @@
 public class Calculator {
-    public int plus (int a ,int b){
+    public int plus(int a,intb){
         return a + b;
     }
-
     public int minus (int a,int b){
         return a + b;
     }
